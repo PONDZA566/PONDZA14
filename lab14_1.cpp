937dc2ed54f9c640220d35bcf493656908aed4b7
Sarah Smith <sarah.smith@email.com>
2023-01-13 17:23:52 +0700

    lab14_completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,25 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum,std_sum=0,std_dev=0,pow_sum = 1,sum_harmonic=0,max= A[0],min= A[0];
+    for(int i=0 ; i < N ; i++){
+        sum += A[i];
+        pow_sum *= A[i];
+        sum_harmonic += 1.0/A[i];
+        if(A[i] > max) max=A[i];
+        if(A[i] < min) min=A[i];
+    }
+    B[0] = sum/N;
+    for(int i=0 ; i < N ; i++){
+        std_sum += (A[i] - B[0])*(A[i] - B[0]);
+    }
+    std_dev = sqrt(std_sum/N);
+    B[1] = std_dev;
+    B[2] = pow(pow_sum,1.0/N);
+    B[3] = N/sum_harmonic;
+    B[4] = max;
+    B[5] = min;
+
+}
